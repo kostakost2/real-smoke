a2691a87bdbef39e6bfd545ee6dffabc742be938
kostakost2 <119595598+kostakost2@users.noreply.github.com>
2026-02-08 00:13:36 +0100

    Add lua54 and escrow_ignore for Asset Escrow compatibility

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'kostakost'
 description 'Real Smoke - Realistic Vehicle Fire Smoke for FiveM'
 version '1.0.0'
+
+escrow_ignore {
+    'config.lua'
+}
 
 shared_scripts {
     'config.lua'
